18c2e9766867759d823a433a7c928b1a640ed642
Ryan Jin <ryanjin333@gmail.com>
2022-10-25 23:40:08 -0700

    Functional Date

--- src/HomeScreen/Calendar/index.jsx
+++ src/HomeScreen/Calendar/index.jsx
@@ -1,15 +1,20 @@
-import { View, Text, Image, ImageBackground } from "react-native";
+import { View, Text, ImageBackground } from "react-native";
+import moment from "moment";
 import styles from "./styles";
 
 const Calendar = () => {
+
+    var month = moment().format('ddd');
+    var date = moment().format('D');
+
     return (
         <View style={styles.calendarContainer}>
             <ImageBackground source={require('../../../assets/Day.png')} style={styles.dayCircle}>
                 <Text style={styles.dayText}>Day 1</Text>
             </ImageBackground>
             <ImageBackground source={require('../../../assets/Date.png')} style={styles.dateCircle}>
-                <Text style={styles.topDateText}>Wed</Text>
-                <Text style={styles.bottomDateText}>24</Text>
+                <Text style={styles.topDateText}>{month}</Text>
+                <Text style={styles.bottomDateText}>{date}</Text>
             </ImageBackground>
             <ImageBackground source={require('../../../assets/Decoration.png')} style={styles.decorationCircle} />
         </View>
